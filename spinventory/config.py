e18ed93b6427481d9e0cb14d2416dd768147402a
Braisdlim <150258751+Braisdlim@users.noreply.github.com>
2025-05-13 23:55:02 +0200

    Delete spinventory directory

--- spinventory/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-muy-segura'
-    SIROPE_DIR = 'data'  # Directorio para almacenamiento local
-    # No necesitas configurar redis_url en esta versión
